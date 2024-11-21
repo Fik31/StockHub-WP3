e96e4c2a7ef15efdf7860a990a6f9eb5e0a1792a
Fik31 <fikrian.famz31@gmail.com>
2024-11-21 14:17:15 +0700

    change: tidy up controller

--- application/controllers/Orders.php
+++ application/controllers/Orders.php
@@ -1,17 +1,14 @@
 <?php
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Orders extends Admin_Controller 
+class Orders extends Admin_Controller
 {
 	public function __construct()
 	{
 		parent::__construct();
-
 		$this->not_logged_in();
-
 		$this->data['page_title'] = 'Orders';
-
 		$this->load->model('model_orders');
 		$this->load->model('model_products');
 		$this->load->model('model_company');
@@ -19,50 +16,38 @@ class Orders extends Admin_Controller
 
 	public function index()
 	{
-		if(!in_array('viewOrder', $this->permission)) {
-            redirect('dashboard', 'refresh');
-        }
-
+		if (!in_array('viewOrder', $this->permission)) {
+			redirect('dashboard', 'refresh');
+		}
 		$this->data['page_title'] = 'Orders';
-		$this->render_template('orders/index', $this->data);		
+		$this->render_template('orders/index', $this->data);
 	}
-	
+
 	public function fetchOrdersData()
 	{
 		$result = array('data' => array());
-
 		$data = $this->model_orders->getOrdersData();
-
 		foreach ($data as $key => $value) {
-
 			$count_total_item = $this->model_orders->countOrderItem($value['id']);
 			$date = date('d-m-Y', $value['date_time']);
 			$time = date('h:i a', $value['date_time']);
-
 			$date_time = $date . ' ' . $time;
-			
 			// button
 			$buttons = '';
-
-			if(in_array('viewOrder', $this->permission)) {
-				$buttons .= '<center><a target="__blank" href="'.base_url('Orders/printDiv/'.$value['id']).'" class="btn btn-primary btn-sm"><i class="fa fa-print"></i></a>';
+			if (in_array('viewOrder', $this->permission)) {
+				$buttons .= '<center><a target="__blank" href="' . base_url('Orders/printDiv/' . $value['id']) . '" class="btn btn-primary btn-sm"><i class="fa fa-print"></i></a>';
 			}
-
-			if(in_array('updateOrder', $this->permission)) {
-				$buttons .= ' <a href="'.base_url('Orders/update/'.$value['id']).'" class="btn btn-warning btn-sm"><i class="fa fa-pencil"></i></a>';
+			if (in_array('updateOrder', $this->permission)) {
+				$buttons .= ' <a href="' . base_url('Orders/update/' . $value['id']) . '" class="btn btn-warning btn-sm"><i class="fa fa-pencil"></i></a>';
 			}
-
-			if(in_array('deleteOrder', $this->permission)) {
-				$buttons .= ' <button type="button" class="btn btn-danger btn-sm" onclick="removeFunc('.$value['id'].')" data-toggle="modal" data-target="#removeModal"><i class="fa fa-trash"></i></button></center>';
+			if (in_array('deleteOrder', $this->permission)) {
+				$buttons .= ' <button type="button" class="btn btn-danger btn-sm" onclick="removeFunc(' . $value['id'] . ')" data-toggle="modal" data-target="#removeModal"><i class="fa fa-trash"></i></button></center>';
 			}
-
-			if($value['paid_status'] == 1) {
-				$paid_status = '<center><span class="label label-success">Paid</span></center>';	
-			}
-			else {
+			if ($value['paid_status'] == 1) {
+				$paid_status = '<center><span class="label label-success">Paid</span></center>';
+			} else {
 				$paid_status = '<center><span class="label label-warning">Not Paid</span></center>';
 			}
-
 			$result['data'][$key] = array(
 				$value['bill_no'],
 				$value['customer_name'],
@@ -71,57 +56,46 @@ class Orders extends Admin_Controller
 				$date_time,
 				$count_total_item,
 				// '$'.$value['net_amount'],
-				'Rp '.number_format($value['net_amount'], 0, ',', '.'),
+				'Rp ' . number_format($value['net_amount'], 0, ',', '.'),
 				$paid_status,
 				$buttons
 			);
 		} // /foreach
-
 		echo json_encode($result);
 	}
 
 	public function create()
 	{
-		if(!in_array('createOrder', $this->permission)) {
-            redirect('dashboard', 'refresh');
-        }
-
+		if (!in_array('createOrder', $this->permission)) {
+			redirect('dashboard', 'refresh');
+		}
 		$this->data['page_title'] = 'Orders';
-
 		$this->form_validation->set_rules('product[]', 'Product name', 'trim|required');
 		$this->form_validation->set_rules('customer_address', 'Customer Address', 'trim|required');
-		
-	
-        if ($this->form_validation->run() == TRUE) {        	
-        	
-        	$order_id = $this->model_orders->create();
-        	
-        	if($order_id) {
-        		$this->session->set_flashdata('success', 'Successfully created');
-        		redirect('Orders/update/'.$order_id, 'refresh');
-        	}
-        	else {
-        		$this->session->set_flashdata('errors', 'Error occurred!!');
-        		redirect('Orders/create/', 'refresh');
-        	}
-        }
-        else {
-            // false case
-        	$company = $this->model_company->getCompanyData(1);
-        	$this->data['company_data'] = $company;
-        	$this->data['is_vat_enabled'] = ($company['vat_charge_value'] > 0) ? true : false;
-        	$this->data['is_service_enabled'] = ($company['service_charge_value'] > 0) ? true : false;
-
-        	$this->data['products'] = $this->model_products->getActiveProductData();      	
-
-            $this->render_template('orders/create', $this->data);
-        }	
+		if ($this->form_validation->run() == TRUE) {
+			$order_id = $this->model_orders->create();
+			if ($order_id) {
+				$this->session->set_flashdata('success', 'Successfully created');
+				redirect('Orders/update/' . $order_id, 'refresh');
+			} else {
+				$this->session->set_flashdata('errors', 'Error occurred!!');
+				redirect('Orders/create/', 'refresh');
+			}
+		} else {
+			// false case
+			$company = $this->model_company->getCompanyData(1);
+			$this->data['company_data'] = $company;
+			$this->data['is_vat_enabled'] = ($company['vat_charge_value'] > 0) ? true : false;
+			$this->data['is_service_enabled'] = ($company['service_charge_value'] > 0) ? true : false;
+			$this->data['products'] = $this->model_products->getActiveProductData();
+			$this->render_template('orders/create', $this->data);
+		}
 	}
 
 	public function getProductValueById()
 	{
 		$product_id = $this->input->post('product_id');
-		if($product_id) {
+		if ($product_id) {
 			$product_data = $this->model_products->getProductData($product_id);
 			echo json_encode($product_data);
 		}
@@ -135,99 +109,76 @@ class Orders extends Admin_Controller
 
 	public function update($id)
 	{
-		if(!in_array('updateOrder', $this->permission)) {
-            redirect('dashboard', 'refresh');
-        }
-
-		if(!$id) {
+		if (!in_array('updateOrder', $this->permission)) {
 			redirect('dashboard', 'refresh');
 		}
-
+		if (!$id) {
+			redirect('dashboard', 'refresh');
+		}
 		$this->data['page_title'] = 'Update Order';
-
 		$this->form_validation->set_rules('product[]', 'Product name', 'trim|required');
-		
-	
-        if ($this->form_validation->run() == TRUE) {        	
-        	
-        	$update = $this->model_orders->update($id);
-        	
-        	if($update == true) {
-        		$this->session->set_flashdata('success', 'Successfully updated');
-        		redirect('Orders/update/'.$id, 'refresh');
-        	}
-        	else {
-        		$this->session->set_flashdata('errors', 'Error occurred!!');
-        		redirect('Orders/update/'.$id, 'refresh');
-        	}
-        }
-        else {
-            // false case
-        	$company = $this->model_company->getCompanyData(1);
-        	$this->data['company_data'] = $company;
-        	$this->data['is_vat_enabled'] = ($company['vat_charge_value'] > 0) ? true : false;
-        	$this->data['is_service_enabled'] = ($company['service_charge_value'] > 0) ? true : false;
-
-        	$result = array();
-        	$orders_data = $this->model_orders->getOrdersData($id);
-
-    		$result['order'] = $orders_data;
-    		$orders_item = $this->model_orders->getOrdersItemData($orders_data['id']);
-
-    		foreach($orders_item as $k => $v) {
-    			$result['order_item'][] = $v;
-    		}
-
-    		$this->data['order_data'] = $result;
-
-        	$this->data['products'] = $this->model_products->getActiveProductData();      	
-
-            $this->render_template('orders/edit', $this->data);
-        }
+		if ($this->form_validation->run() == TRUE) {
+			$update = $this->model_orders->update($id);
+			if ($update == true) {
+				$this->session->set_flashdata('success', 'Successfully updated');
+				redirect('Orders/update/' . $id, 'refresh');
+			} else {
+				$this->session->set_flashdata('errors', 'Error occurred!!');
+				redirect('Orders/update/' . $id, 'refresh');
+			}
+		} else {
+			// false case
+			$company = $this->model_company->getCompanyData(1);
+			$this->data['company_data'] = $company;
+			$this->data['is_vat_enabled'] = ($company['vat_charge_value'] > 0) ? true : false;
+			$this->data['is_service_enabled'] = ($company['service_charge_value'] > 0) ? true : false;
+			$result = array();
+			$orders_data = $this->model_orders->getOrdersData($id);
+			$result['order'] = $orders_data;
+			$orders_item = $this->model_orders->getOrdersItemData($orders_data['id']);
+			foreach ($orders_item as $k => $v) {
+				$result['order_item'][] = $v;
+			}
+			$this->data['order_data'] = $result;
+			$this->data['products'] = $this->model_products->getActiveProductData();
+			$this->render_template('orders/edit', $this->data);
+		}
 	}
 
 	public function remove()
 	{
-		if(!in_array('deleteOrder', $this->permission)) {
-            redirect('dashboard', 'refresh');
-        }
-
+		if (!in_array('deleteOrder', $this->permission)) {
+			redirect('dashboard', 'refresh');
+		}
 		$order_id = $this->input->post('order_id');
-
-        $response = array();
-        if($order_id) {
-            $delete = $this->model_orders->remove($order_id);
-            if($delete == true) {
-                $response['success'] = true;
-                $response['messages'] = "Successfully removed"; 
-            }
-            else {
-                $response['success'] = false;
-                $response['messages'] = "Error in the database while removing the product information";
-            }
-        }
-        else {
-            $response['success'] = false;
-            $response['messages'] = "Refersh the page again!!";
-        }
-
-        echo json_encode($response); 
+		$response = array();
+		if ($order_id) {
+			$delete = $this->model_orders->remove($order_id);
+			if ($delete == true) {
+				$response['success'] = true;
+				$response['messages'] = "Successfully removed";
+			} else {
+				$response['success'] = false;
+				$response['messages'] = "Error in the database while removing the product information";
+			}
+		} else {
+			$response['success'] = false;
+			$response['messages'] = "Refersh the page again!!";
+		}
+		echo json_encode($response);
 	}
 
 	public function printDiv($id)
 	{
-		if(!in_array('viewOrder', $this->permission)) {
-            redirect('dashboard', 'refresh');
-        }
-        
-		if($id) {
+		if (!in_array('viewOrder', $this->permission)) {
+			redirect('dashboard', 'refresh');
+		}
+		if ($id) {
 			$order_data = $this->model_orders->getOrdersData($id);
 			$orders_items = $this->model_orders->getOrdersItemData($id);
 			$company_info = $this->model_company->getCompanyData(1);
-
 			$order_date = date('d/m/Y', $order_data['date_time']);
 			$paid_status = ($order_data['paid_status'] == 1) ? "Paid" : "Unpaid";
-
 			$html = '<!-- Main content -->
 			<!DOCTYPE html>
 			<html>
@@ -238,10 +189,10 @@ class Orders extends Admin_Controller
 			  <!-- Tell the browser to be responsive to screen width -->
 			  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 			  <!-- Bootstrap 3.3.7 -->
-			  <link rel="stylesheet" href="'.base_url('assets/bower_components/bootstrap/dist/css/bootstrap.min.css').'">
+			  <link rel="stylesheet" href="' . base_url('assets/bower_components/bootstrap/dist/css/bootstrap.min.css') . '">
 			  <!-- Font Awesome -->
-			  <link rel="stylesheet" href="'.base_url('assets/bower_components/font-awesome/css/font-awesome.min.css').'">
-			  <link rel="stylesheet" href="'.base_url('assets/dist/css/AdminLTE.min.css').'">
+			  <link rel="stylesheet" href="' . base_url('assets/bower_components/font-awesome/css/font-awesome.min.css') . '">
+			  <link rel="stylesheet" href="' . base_url('assets/dist/css/AdminLTE.min.css') . '">
 			</head>
 			<body onload="window.print();">
 			
@@ -251,8 +202,8 @@ class Orders extends Admin_Controller
 			    <div class="row">
 			      <div class="col-xs-12">
 			        <h2 class="page-header">
-			          '.$company_info['company_name'].'
-			          <small class="pull-right">Date: '.$order_date.'</small>
+			          ' . $company_info['company_name'] . '
+			          <small class="pull-right">Date: ' . $order_date . '</small>
 			        </h2>
 			      </div>
 			      <!-- /.col -->
@@ -262,10 +213,10 @@ class Orders extends Admin_Controller
 			      
 			      <div class="col-sm-4 invoice-col">
 			        
-			        <b>Bill ID:</b> '.$order_data['bill_no'].'<br>
-			        <b>Customer:</b> '.$order_data['customer_name'].'<br>
-			        <b>Address:</b> '.$order_data['customer_address'].' <br />
-			        <b>Contact:</b> '.$order_data['customer_phone'].'
+			        <b>Bill ID:</b> ' . $order_data['bill_no'] . '<br>
+			        <b>Customer:</b> ' . $order_data['customer_name'] . '<br>
+			        <b>Address:</b> ' . $order_data['customer_address'] . ' <br />
+			        <b>Contact:</b> ' . $order_data['customer_phone'] . '
 			      </div>
 			      <!-- /.col -->
 			    </div>
@@ -283,21 +234,19 @@ class Orders extends Admin_Controller
 			            <th>Amount</th>
 			          </tr>
 			          </thead>
-			          <tbody>'; 
+			          <tbody>';
 
-			          foreach ($orders_items as $k => $v) {
-
-			          	$product_data = $this->model_products->getProductData($v['product_id']); 
-			          	
-			          	$html .= '<tr>
-				            <td>'.$product_data['name'].'</td>
+			foreach ($orders_items as $k => $v) {
+				$product_data = $this->model_products->getProductData($v['product_id']);
+				$html .= '<tr>
+				            <td>' . $product_data['name'] . '</td>
 							<td>Rp ' . number_format($v['rate'], 0, ',', '.') . '</td>
-				            <td>'.$v['qty'].'</td>
+				            <td>' . $v['qty'] . '</td>
 							<td>Rp ' . number_format($v['amount'], 0, ',', '.') . '</td>
 			          	</tr>';
-			          }
-			          
-			          $html .= '</tbody>
+			}
+
+			$html .= '</tbody>
 			        </table>
 			      </div>
 			      <!-- /.col -->
@@ -315,22 +264,22 @@ class Orders extends Admin_Controller
 			              <td>Rp ' . number_format(floatval($order_data['gross_amount']), 0, ',', '.') . '</td>
 			            </tr>';
 
-			            if($order_data['service_charge'] > 0) {
-			            	$html .= '<tr>
-				              <th>Service Charge ('.$order_data['service_charge_rate'].'%)</th>
+			if ($order_data['service_charge'] > 0) {
+				$html .= '<tr>
+				              <th>Service Charge (' . $order_data['service_charge_rate'] . '%)</th>
 				              <td>Rp ' . number_format(floatval($order_data['service_charge']), 0, ',', '.') . '</td>
 				            </tr>';
-			            }
+			}
 
-			            if($order_data['vat_charge'] > 0) {
-			            	$html .= '<tr>
-				              <th>Vat Charge ('.$order_data['vat_charge_rate'].'%)</th>
+			if ($order_data['vat_charge'] > 0) {
+				$html .= '<tr>
+				              <th>Vat Charge (' . $order_data['vat_charge_rate'] . '%)</th>
 				              <td>Rp ' . number_format(floatval($order_data['vat_charge']), 0, ',', '.') . '</td>
 				            </tr>';
-			            }
-			            
-			            
-			            $html .=' <tr>
+			}
+
+
+			$html .= ' <tr>
 			              <th>Discount:</th>
 			              <td>Rp ' . number_format(floatval($order_data['discount']), 0, ',', '.') . '</td>
 			            </tr>
@@ -340,7 +289,7 @@ class Orders extends Admin_Controller
 			            </tr>
 			            <tr>
 			              <th>Bill Status:</th>
-			              <td>'.$paid_status.'</td>
+			              <td>' . $paid_status . '</td>
 			            </tr>
 			          </table>
 			        </div>
@@ -354,8 +303,7 @@ class Orders extends Admin_Controller
 		</body>
 	</html>';
 
-			  echo $html;
+			echo $html;
 		}
 	}
-
 }
--- application/controllers/Permission.php
+++ application/controllers/Permission.php
@@ -1,156 +1,113 @@
-<?php 
+<?php
 
-class Permission extends Admin_Controller 
+class Permission extends Admin_Controller
 {
 	public function __construct()
 	{
 		parent::__construct();
-
 		$this->not_logged_in();
-
 		$this->data['page_title'] = 'Permission';
-		
-
 		$this->load->model('model_groups');
 	}
 
-	/* 
-	* It redirects to the manage group page
-	* As well as the group data is also been passed to display on the view page
-	*/
 	public function index()
 	{
-
-		if(!in_array('viewGroup', $this->permission)) {
+		if (!in_array('viewGroup', $this->permission)) {
 			redirect('dashboard', 'refresh');
 		}
-
 		$groups_data = $this->model_groups->getGroupData();
 		$this->data['groups_data'] = $groups_data;
-
 		$this->render_template('permission/index', $this->data);
-	}	
-
-	/*
-	* If the validation is not valid, then it redirects to the create page.
-	* If the validation is for each input field is valid then it inserts the data into the database 
-	* and it stores the operation message into the session flashdata and display on the manage group page
-	*/
-	public function create()
-	{
-
-		if(!in_array('createGroup', $this->permission)) {
-			redirect('dashboard', 'refresh');
-		}
-
-		$this->form_validation->set_rules('group_name', 'Permission name', 'required');
-
-        if ($this->form_validation->run() == TRUE) {
-            // true case
-            $permission = serialize($this->input->post('permission'));
-            
-        	$data = array(
-        		'group_name' => $this->input->post('group_name'),
-        		'permission' => $permission
-        	);
-
-        	$create = $this->model_groups->create($data);
-        	if($create == true) {
-        		$this->session->set_flashdata('success', 'Successfully created');
-        		redirect('Permission/', 'refresh');
-        	}
-        	else {
-        		$this->session->set_flashdata('errors', 'Error occurred!!');
-        		redirect('Permission/create', 'refresh');
-        	}
-        }
-        else {
-            // false case
-            $this->render_template('permission/create', $this->data);
-        }	
 	}
 
-	/*
-	* If the validation is not valid, then it redirects to the edit group page 
-	* If the validation is successfully then it updates the data into the database 
-	* and it stores the operation message into the session flashdata and display on the manage group page
-	*/
-	public function edit($id = null)
+	public function create()
 	{
-
-		if(!in_array('updateGroup', $this->permission)) {
+		if (!in_array('createGroup', $this->permission)) {
 			redirect('dashboard', 'refresh');
 		}
+		$this->form_validation->set_rules('group_name', 'Permission name', 'required');
+		if ($this->form_validation->run() == TRUE) {
+			// true case
+			$permission = serialize($this->input->post('permission'));
+			$data = array(
+				'group_name' => $this->input->post('group_name'),
+				'permission' => $permission
+			);
+			$create = $this->model_groups->create($data);
+			if ($create == true) {
+				$this->session->set_flashdata('success', 'Successfully created');
+				redirect('Permission/', 'refresh');
+			} else {
+				$this->session->set_flashdata('errors', 'Error occurred!!');
+				redirect('Permission/create', 'refresh');
+			}
+		} else {
+			// false case
+			$this->render_template('permission/create', $this->data);
+		}
+	}
 
-		if($id) {
-
+	public function edit($id = null)
+	{
+		if (!in_array('updateGroup', $this->permission)) {
+			redirect('dashboard', 'refresh');
+		}
+		if ($id) {
 			$this->form_validation->set_rules('group_name', 'Permission name', 'required');
 
 			if ($this->form_validation->run() == TRUE) {
-	            // true case
-	            $permission = serialize($this->input->post('permission'));
-	            
-	        	$data = array(
-	        		'group_name' => $this->input->post('group_name'),
-	        		'permission' => $permission
-	        	);
-
-	        	$update = $this->model_groups->edit($data, $id);
-	        	if($update == true) {
-	        		$this->session->set_flashdata('success', 'Successfully updated');
-	        		redirect('Permission/', 'refresh');
-	        	}
-	        	else {
-	        		$this->session->set_flashdata('errors', 'Error occurred!!');
-	        		redirect('Permission/edit/'.$id, 'refresh');
-	        	}
-	        }
-	        else {
-	            // false case
-	            $group_data = $this->model_groups->getGroupData($id);
+				// true case
+				$permission = serialize($this->input->post('permission'));
+				$data = array(
+					'group_name' => $this->input->post('group_name'),
+					'permission' => $permission
+				);
+				$update = $this->model_groups->edit($data, $id);
+				if ($update == true) {
+					$this->session->set_flashdata('success', 'Successfully updated');
+					redirect('Permission/', 'refresh');
+				} else {
+					$this->session->set_flashdata('errors', 'Error occurred!!');
+					redirect('Permission/edit/' . $id, 'refresh');
+				}
+			} else {
+				// false case
+				$group_data = $this->model_groups->getGroupData($id);
 				$this->data['group_data'] = $group_data;
-				$this->render_template('permission/edit', $this->data);	
-	        }	
+				$this->render_template('permission/edit', $this->data);
+			}
 		}
 	}
 
-	/*
-	* It removes the removes information from the database 
-	* and it stores the operation message into the session flashdata and display on the manage group page
-	*/
+
 	public function delete($id)
 	{
 
-		if(!in_array('deleteGroup', $this->permission)) {
+		if (!in_array('deleteGroup', $this->permission)) {
 			redirect('dashboard', 'refresh');
 		}
 
-		if($id) {
-			if($this->input->post('confirm')) {
+		if ($id) {
+			if ($this->input->post('confirm')) {
 
 				$check = $this->model_groups->existInUserGroup($id);
-				if($check == true) {
+				if ($check == true) {
 					$this->session->set_flashdata('error', 'Group exists in the users');
-	        		redirect('Permission/', 'refresh');
-				}
-				else {
+					redirect('Permission/', 'refresh');
+				} else {
 					$delete = $this->model_groups->delete($id);
-					if($delete == true) {
-		        		$this->session->set_flashdata('success', 'Successfully removed');
-		        		redirect('Permission/', 'refresh');
-		        	}
-		        	else {
-		        		$this->session->set_flashdata('error', 'Error occurred!!');
-		        		redirect('Permission/delete/'.$id, 'refresh');
-		        	}
-				}	
-			}	
-			else {
+					if ($delete == true) {
+						$this->session->set_flashdata('success', 'Successfully removed');
+						redirect('Permission/', 'refresh');
+					} else {
+						$this->session->set_flashdata('error', 'Error occurred!!');
+						redirect('Permission/delete/' . $id, 'refresh');
+					}
+				}
+			} else {
 				$this->data['id'] = $id;
 				$this->render_template('permission/delete', $this->data);
-			}	
+			}
 		}
 	}
-
-
 }
